81b3cab70b870f60258a6d4d91765a20422b974b
your name here <echen831@gmail.com>
2020-05-20 10:41:02 -0700

    add target basket on page

--- src/index.js
+++ src/index.js
@@ -19,7 +19,21 @@ document.addEventListener('DOMContentLoaded', ()=> {
     Util.addFruits(FRUITS); // Use addFruits method from Util file to add fruits to page.
 
     let fruits = Object.values(FRUITS); // Generate an array of 4 random fruits using random fruits from Util file.
-    Util.randomFruit(fruits);
+    let randomFruits = Util.randomFruit(fruits);
+
+    const targetFruits = (arr) => {
+        arr.forEach(ele => {
+            let basket = document.getElementById('target-basket');
+            let item = document.createElement('div');
+            item.setAttribute('class', 'target-fruit');
+            let pic = document.createTextNode(ele);
+            item.appendChild(pic);
+            basket.appendChild(item);
+        })
+    }
+
+    targetFruits(randomFruits)
+
 
     const board = new Board() // creating a new instance of board.
 
