ebafba502741a2bc9c303c22b78bc49235c13c67
your name here <echen831@gmail.com>
2020-08-28 20:29:24 -0700

    update controls

--- src/game.js
+++ src/game.js
@@ -32,6 +32,7 @@ class Game {
     reset () {
         this.lvl = 1;
         this.streak = 0;
+        resetTimer();
     }
 
     pause() {
@@ -135,43 +136,6 @@ class Game {
             }
             Util.addFruit()      
         }
-
-        // switch (e.keyCode) {
-        //     case 37:
-        //         if (parseInt(circle.style.left) - 25 > 0) {
-        //             circle.style.left = parseInt(circle.style.left) - moveBy
-        //         } else {
-        //             circle.style.left = 0
-        //         }
-        //         Util.addFruit()
-        //         break;
-        //     case 39:
-        //         if (parseInt(circle.style.left) + 25 < 400) {
-        //             circle.style.left = parseInt(circle.style.left) + moveBy
-        //         } else {
-        //             circle.style.left = (400 - 25)
-        //         }
-        //         Util.addFruit()
-        //         break;
-        //     case 38:
-        //         if (parseInt(circle.style.top) - 25 > 0) {
-        //             circle.style.top = parseInt(circle.style.top) - moveBy
-        //         } else {
-        //             circle.style.top = 0
-        //         }
-        //         Util.addFruit()
-        //         break;
-        //     case 40:
-        //         if (parseInt(circle.style.top) + 25 < 400) {
-        //             circle.style.top = parseInt(circle.style.top) + moveBy
-        //         } else {
-        //             circle.style.top = (400 - 25)
-        //         }
-        //         Util.addFruit()
-        //         break;
-        //     default:
-        //         break;
-        // }
 
     }
 
